46260d15b461732f0a57d51a65bf7e4598bec4e6
unknown <muralireddypatil@gmail.com>
2025-03-19 13:13:20 +0000

    productdb database and products table is created

--- DIDElectrnoicStore/js/main.js
+++ DIDElectrnoicStore/js/main.js
@@ -1,14 +1,6 @@
-const db = new Dexie('myDB');
-db.version(1).stores({
-    friends: '++id, name, age' // Fixed schema definition
-});
 
-// Open database
-db.open().catch(err => {
-    console.error("Failed to open DB:", err);
-});
+import productdb from "./module";
 
-// Add data to test if it works
-db.friends.put({ name: 'John', age: 25 }).then(() => {
-    console.log("Data added successfully!");
-});
+productdb("Productdb", {
+    products: '++id, name, seller, price'
+} );
--- DIDElectrnoicStore/js/module.js
+++ DIDElectrnoicStore/js/module.js
@@ -1,11 +1,28 @@
 
-//Create database
-// const db = new Dexie ('myDB');
+
+const productdb = (dbname, table)=>{
+//     const db = new Dexie('myDB');
 // db.version(1).stores({
-//     friends: 'name , age'
-// })
-// db.open();
+//     friends: '++id, name, age' // Fixed schema definition
+// });
+
+// // Open database
+// db.open().catch(err => {
+//     console.error("Failed to open DB:", err);
+// });
+
+// // Add data to test if it works
+// db.friends.put({ name: 'John', age: 25 }).then(() => {
+//     console.log("Data added successfully!");
+// });
+
+const db = Dexie(dbname);
+db.version(1).stores(table);
+
+db.open();
+
+return db;
+}
 
 
-// Create database
-
+export default productdb;
