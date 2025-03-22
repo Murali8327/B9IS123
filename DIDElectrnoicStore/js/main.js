abb0aead27d5daff1aef57672bdd1b107f90edf7
unknown <muralireddypatil@gmail.com>
2025-03-22 13:19:54 +0000

    inserted value using create button in main.js

--- DIDElectrnoicStore/js/main.js
+++ DIDElectrnoicStore/js/main.js
@@ -17,3 +17,12 @@ const btnread=document.getElementById("btnread");
 const btnupdate=document.getElementById(" btnupdate");
 const btndelete=document.getElementById("btndelete");
 
+//insert value using create button
+btncreate.onclick=(event)=>{
+ let flag= bulkcreate(db.products,{
+    name:proname.value,
+    seller:seller,value,
+    price:price.value,
+  })
+  console.log(flag);
+}
